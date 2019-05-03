cf9a2ff1c692f46e8f056f76c1ac15894ca57e69
Doshinsky <akter1997@gmail.com>
2019-05-03 22:07:03 +0700

    Added restructed project (Vol.3)

---
+++ Vol.3/include/glist.h
@@ -0,0 +1,27 @@
+#ifndef GLIST_H
+#define GLIST_H
+
+typedef struct glist glist;
+typedef struct glroot glroot;
+
+struct glroot {
+  int count;
+  struct glist *first_node;
+};
+
+struct glist {
+  int number;
+  int fd;
+  int chk_fd;
+  int flg;
+  int field[3][3];
+  struct glist *ptr;
+};
+
+void glistprint(glroot *root);
+struct glroot *ginit();
+struct glist *gaddelem(glroot *root, int number, int sockfd);
+struct glist *gdeletelem(glist *lst, glroot *root);
+struct glist *glistfind(glroot *root, int number);
+
+#endif
